af29645ef59186d2b1df736ac9755d05b751f8dc
Kape98 <kn-nero@hotmail.com>
2021-06-21 02:28:37 +0300

    added dictionaries and started writing the algorithm

--- bot/melvor.py
+++ bot/melvor.py
@@ -1,12 +1,29 @@
 import discord
+import random
 from discord.ext import commands
+from monsterlist import monsterDic, aliases
 
 client = commands.Bot(command_prefix=';')
+
+odds = 0.0
+droplist = []
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+
+@client.command
+async def slay(ctx, monster, attempts):
+    if monster < 0 or monster > 2:
+        print('invalid monster')
+        return
+
+    for i in range(attempts):
+        odds = random.uniform(0.00, 100.0)
+        monsterDic.get(monster)
+        return
 
 
 client.run('ODU2Mjc4MTAwNzE4NTE4MzAy.YM-tIA.jJ9AyjhqP0pbJhKaiUD-JqRk1Z8')
---
+++ bot/monsterlist.py
@@ -0,0 +1,28 @@
+monsterDic = {'Black Knight': {
+    'Black Boots': [2.45, 1],
+    'Black Helmet': [1.47, 1],
+    'Black Dagger': [1.47, 1],
+    'Black 2H Sword': [0.98, 1],
+    'Black Battleaxe': [0.98, 1],
+    'Black Sword': [0.98, 1],
+    'Black Shield': [0.98, 1],
+    'Black Scimitar': [0.39, 1],
+    'Black Platelegs': [0.25, 1],
+    'Black Platebody': [0.05, 1],
+},
+    'Hill Giant': {
+        'Iron Arrows': [18.25, 15],
+        'Mantalyme Seeds': [18.25, 1],
+        'Onion Seeds': [18.25, 10],
+        'Cabbage Seed': [18.25, 10],
+        'Steel Arrows': [18.25, 15],
+        'Watermelon Seeds': [7.3, 5],
+        'Sapphire': [0.73, 1],
+        'Ruby': [0.73, 1],
+    }
+}
+
+aliases = {
+    'Black Knight': 0,
+    'Hill Giant': 1
+}
