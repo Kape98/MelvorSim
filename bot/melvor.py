580a38b2859f4fec254c2da3a78b19dccdabc83c
Kape98 <kn-nero@hotmail.com>
2021-06-21 05:32:07 +0300

    added chat send

--- bot/melvor.py
+++ bot/melvor.py
@@ -8,7 +8,7 @@ client = commands.Bot(command_prefix=';')
 
 currentMonster = {}
 droplist = []
-
+attempts = 10
 
 @client.event
 async def on_ready():
@@ -17,17 +17,16 @@ async def on_ready():
 
 @client.command
 async def slay(ctx, monster, attempts):
-    if monster < 0 or monster > 2:
-        print('invalid monster')
-        return
+    #if monster < 0 or monster > 2:
+     #   print('invalid monster')
+      #  return
 
     for i in range(attempts):
-        odds = random.uniform(0.00, 100.0)
         droplist.append(np.random.choice(monsterDic.keys(), attempts, replace=True, p=monsterDic.values()))
         print(droplist)
-        await ctx.send(droplist)
+        #await ctx.send(droplist)
 
-        return
+
 
 
 client.run('ODU2Mjc4MTAwNzE4NTE4MzAy.YM-tIA.jJ9AyjhqP0pbJhKaiUD-JqRk1Z8')
--- test/main.py
+++ test/main.py
@@ -1,4 +1,8 @@
-
+monster = 'Black Knight'
+attempts = 10
+import random
+import pprint
+import numpy as np
 
 BlackKnightDic = {
     'Black Boots': [0.0245, 1],
@@ -13,9 +17,19 @@ BlackKnightDic = {
     'Black Platebody': [0.0005, 1],
 }
 
-
-
-
+BlackKnightDic1 = {
+    'Black Boots': 0.0245,
+    'Black Helmet': 0.0147,
+    'Black Dagger': 0.0147,
+    'Black 2H Sword': 0.0098,
+    'Black Battleaxe': 0.0098,
+    'Black Sword': 0.0098,
+    'Black Shield': 0.0098,
+    'Black Scimitar': 0.0039,
+    'Black Platelegs': 0.0025,
+    'Black Platebody': 0.0005,
+    'Nothing': 0.9
+}
 
 HillGiantDic = {
     'Iron Arrows': [0.1825, 15],
@@ -35,4 +49,14 @@ AlwaysDropsDic = {
 
 MonsterDic = {'Black Knight': BlackKnightDic,
               'Hill Giant': HillGiantDic
-}
+              }
+drops = {}
+drop = 0
+droplist = random.choices(list(BlackKnightDic1.keys()), weights=BlackKnightDic1.values(), k=1000)
+#print(droplist)
+for i in range(1000):
+    drops[droplist[i]] = drops.get(droplist[i], 0) + 1
+
+sort_orders = sorted(drops.items(), key=lambda x: x[1], reverse=True)
+pprint.pprint(sort_orders)
+#print(sort_orders)
